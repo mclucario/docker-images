28543455608f9d40c2306444f9f23c18d2b68ab3
Leon Grünewald <l_gruenewald@web.de>
2018-12-20 19:04:35 +0100

    Docker workspace finished

--- php-workspace/initws.sh
+++ php-workspace/initws.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
+echo '========== Workspace StartUp =========='
+
+export NODE_ENABLED="${NODE_VERSION:-false}"
 export NODE_VERSION="${NODE_VERSION:-11.0.0}"
+export BOWER_ENABLED="${NODE_VERSION:-false}"
 export BOWER_VERSION="${BOWER_VERSION:-latest}"
+export WEBPACK_ENABLED="${NODE_VERSION:-false}"
 export WEBPACK_VERSION="${WEBPACK:-latest}"
 export WEBPACK_CLI_VERSION="${WEBPACK_CLI:-latest}"
 
-nvm install $NODE_VERSION
-alias node=/versions/node/v$NODE_VERSION/bin/node
-/opt/nvm/versions/node/v$NODE_VERSION/bin/npm install -g bower@$BOWER_VERSION; \
-/opt/nvm/versions/node/v$NODE_VERSION/bin/npm install -g webpack@$WEBPACK_VERSION webpack-cli@$WEBPACK_CLI_VERSION
+echo "Node Version        : $NODE_VERSION"
+echo "Bower Version       : $BOWER_VERSION"
+echo "Webpack Version     : $WEBPACK_VERSION"
+echo "Webpack CLI Version : $WEBPACK_CLI_VERSION"
+
+if [ NODE_ENABLED = true ] ; then
+    echo '==========  Installing Node   =========='
+
+    export NVM_DIR="/opt/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+    nvm install $NODE_VERSION
+    nvm use $NODE_VERSION
+
+    if [ BOWER_ENABLED = true ] ; then
+        echo '==========  Installing Bower  =========='
+        npm install -g bower@$BOWER_VERSION;
+    else
+        echo "Bower Disabled"
+    fi
+
+    if [ WEBPACK_ENABLED = true ] ; then
+        echo '========== Installing Webpack =========='
+        npm install -g webpack@$WEBPACK_VERSION webpack-cli@$WEBPACK_CLI_VERSION
+    else
+        echo "Webpack Disabled"
+    fi
+
+else
+    echo "Node Disabled"
+fi
+
+finish()
+{
+    rm tmpfiles
+    kill $(cat service.pid)
+    exit
+}
+trap finish SIGINT
+
+while :; do
+    sleep 5
+done
