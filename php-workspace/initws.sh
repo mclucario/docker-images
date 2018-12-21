bc5039670e977de34850451a02d34ed2eb9b76d0
Leon Grünewald <l_gruenewald@web.de>
2018-12-21 13:07:27 +0100

    Clean up env variables and make stuff pretty

--- php-workspace/initws.sh
+++ php-workspace/initws.sh
@@ -2,21 +2,21 @@
 
 echo '========== Workspace StartUp =========='
 
-export NODE_ENABLED="${NODE_VERSION:-false}"
+export NODE_ENABLED="${NODE_ENABLED:-false}"
 export NODE_VERSION="${NODE_VERSION:-11.0.0}"
-export BOWER_ENABLED="${NODE_VERSION:-false}"
+export BOWER_ENABLED="${BOWER_ENABLED:-false}"
 export BOWER_VERSION="${BOWER_VERSION:-latest}"
-export WEBPACK_ENABLED="${NODE_VERSION:-false}"
-export WEBPACK_VERSION="${WEBPACK:-latest}"
-export WEBPACK_CLI_VERSION="${WEBPACK_CLI:-latest}"
+export WEBPACK_ENABLED="${WEBPACK_ENABLED:-false}"
+export WEBPACK_VERSION="${WEBPACK_VERSION:-latest}"
+export WEBPACK_CLI_VERSION="${WEBPACK_CLI_VERSION:-latest}"
 
 echo "Node Version        : $NODE_VERSION"
 echo "Bower Version       : $BOWER_VERSION"
 echo "Webpack Version     : $WEBPACK_VERSION"
 echo "Webpack CLI Version : $WEBPACK_CLI_VERSION"
 
-if [ NODE_ENABLED = true ] ; then
-    echo '==========  Installing Node   =========='
+if [ $NODE_ENABLED = true ] ; then
+    echo '==========  Installing Node  =========='
 
     export NVM_DIR="/opt/nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
@@ -24,14 +24,14 @@ if [ NODE_ENABLED = true ] ; then
     nvm install $NODE_VERSION
     nvm use $NODE_VERSION
 
-    if [ BOWER_ENABLED = true ] ; then
+    if [ $BOWER_ENABLED = true ] ; then
         echo '==========  Installing Bower  =========='
         npm install -g bower@$BOWER_VERSION;
     else
         echo "Bower Disabled"
     fi
 
-    if [ WEBPACK_ENABLED = true ] ; then
+    if [ $WEBPACK_ENABLED = true ] ; then
         echo '========== Installing Webpack =========='
         npm install -g webpack@$WEBPACK_VERSION webpack-cli@$WEBPACK_CLI_VERSION
     else
@@ -42,10 +42,10 @@ else
     echo "Node Disabled"
 fi
 
+echo '==========        DONE        =========='
+
 finish()
 {
-    rm tmpfiles
-    kill $(cat service.pid)
     exit
 }
 trap finish SIGINT
